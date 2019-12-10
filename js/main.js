44fc9c12a9e72d9df88ef2f385f6446376a9e008
Kevin Osborn <kevinosborn31@gmail.com>
2019-12-11 10:37:37 +1100

    fixed a bug where you could click the same square over and over, cleaned up some unnecessary code

--- js/main.js
+++ js/main.js
@@ -23,37 +23,16 @@ $(document).ready(function() {
     reset();
   });
   $('#board li').on('click', function() {
-  //if statement that checks all possible combinations of winning
-    if(spot1.hasClass('o')&&spot2.hasClass('o')&&spot3.hasClass('o') ||
-      spot4.hasClass('o')&&spot5.hasClass('o')&&spot6.hasClass('o') ||
-      spot7.hasClass('o')&&spot8.hasClass('o')&&spot9.hasClass('o') ||
-      spot1.hasClass('o')&&spot4.hasClass('o')&&spot7.hasClass('o') ||
-      spot2.hasClass('o')&&spot5.hasClass('o')&&spot8.hasClass('o') ||
-      spot3.hasClass('o')&&spot6.hasClass('o')&&spot9.hasClass('o') ||
-      spot1.hasClass('o')&&spot5.hasClass('o')&&spot9.hasClass('o') ||
-      spot3.hasClass('o')&&spot5.hasClass('o')&&spot7.hasClass('o'))
-    {
-      alert('Winner: O - Press the reset button to start again');
 
-    } else if(spot1.hasClass('x')&&spot2.hasClass('x')&&spot3.hasClass('x') ||
-      spot4.hasClass('x')&&spot5.hasClass('x')&&spot6.hasClass('x') ||
-      spot7.hasClass('x')&&spot8.hasClass('x')&&spot9.hasClass('x') ||
-      spot1.hasClass('x')&&spot4.hasClass('x')&&spot7.hasClass('x') ||
-      spot2.hasClass('x')&&spot5.hasClass('x')&&spot8.hasClass('x') ||
-      spot3.hasClass('x')&&spot6.hasClass('x')&&spot9.hasClass('x') ||
-      spot1.hasClass('x')&&spot5.hasClass('x')&&spot9.hasClass('x') ||
-      spot3.hasClass('x')&&spot5.hasClass('x')&&spot7.hasClass('x'))
-    {
-      alert('Winner: X - Press the reset button to start again');
-    } else if(turns == 8) {
-      alert('Tie Game - Press the reset button to start again');
-      turns = 0;
-    } else if ($(this).hasClass('disabled')){
+     if(turns == 8) {
+      alert('Tie Game - Press the reset button to start again'); // if turns = 8, and no one has won, its a tie
+      turns = 0; //turns reset to 0
+    } else if ($(this).hasClass('disabled')){ // if you click on a disabled square, it says the spot has been filled
       alert('This spot is already filled');
-    } else if(turns%2 == 0) {
+    } else if(turns%2 == 0) { //if turns are an even number e.g. 2, 4, 6, 8, whichever one is clicked on will be filled with o
       turns++;
-      $(this).text(o);
-      $(this).addClass('disable o');
+      $(this).text(o); // changes the text to "o"
+      $(this).addClass('disabled o'); // adds class "disabled" to the square which means it is taken
       if(spot1.hasClass('o')&&spot2.hasClass('o')&&spot3.hasClass('o') ||
         spot4.hasClass('o')&&spot5.hasClass('o')&&spot6.hasClass('o') ||
         spot7.hasClass('o')&&spot8.hasClass('o')&&spot9.hasClass('o') ||
@@ -66,10 +45,10 @@ $(document).ready(function() {
           turns = 0;
       }
 
-    } else {
+    } else { // if turns are not even, it will be filled with x
       turns++;
-      $(this).text(x);
-      $(this).addClass('disable x');
+      $(this).text(x); // changes the text to "x"
+      $(this).addClass('disabled x'); // adds class "disabled" to the square which means it is taken
       if(spot1.hasClass('x')&&spot2.hasClass('x')&&spot3.hasClass('x') ||
         spot4.hasClass('x')&&spot5.hasClass('x')&&spot6.hasClass('x') ||
         spot7.hasClass('x')&&spot8.hasClass('x')&&spot9.hasClass('x') ||
