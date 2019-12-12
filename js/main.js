1b3083d4caa4a4f75f0f303c5cbf0cc91ff2313f
Kevin Osborn <kevinosborn31@gmail.com>
2019-12-12 15:32:52 +1100

    stopped the game from storing state of the board using DOM, is using an array now

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+let gameBoard = [ // Game board array that stores X or O when you click on a square
+  '', '', '',
+  '', '', '',
+  '', '', ''
+];
 $(document).ready(function() {
   let x = "x";
   let o = "o";
@@ -17,8 +22,10 @@ $(document).ready(function() {
   let scoreOfO = $('#oScore')
   let xScore = 0;
   let oScore = 0;
-  let game = 1;
+  let game = 1; // starting game
   let winner;
+
+
   // let checkForWinner = function () {
   //   if (xScore === 3) {
   //     alert('X wins!');
@@ -30,11 +37,19 @@ $(document).ready(function() {
   //     game = 1;
   //   }
   // }
-  let reset = function () {
+  let reset = function () { // function that resets (removes all classes) the board, is triggered when you click the reset button
       $('#board li').text('-');
       $('#board li').removeClass('disabled');
       $('#board li').removeClass('o');
       $('#board li').removeClass('x');
+
+      gameBoard = [ '', '', '', '', '', '', '', '', '' ];
+
+      // for (i = 0; i < gameBoard.length; i++) { // when you reset, it loops through and resets all values of the array that are X or O and makes them empty
+      //   // if (gameBoard[i] == 'X' || gameBoard[i] == 'O') {
+      //     gameBoard[i] = "";
+      //   // }
+      // }
       // checkForWinner();
       // if (xScore === 3 || oScore === 3) {
       //   xScore = 0;
@@ -42,62 +57,89 @@ $(document).ready(function() {
       // }
       $(gameInfo).text(`Game ${game}`);
   }
+
+  let newGame = function() {
+    game = 1;
+    xScore = 0;
+    oScore = 0;
+    $(scoreOfX).text(`X - ${xScore}`); // displays scores
+    $(scoreOfO).text(`O - ${oScore}`);
+    $(gameInfo).text(`Game ${game}`);
+    alert('Game has been reset');
+  }
+  $("#newGame").on("click", function() {
+    reset();
+    newGame();
+  });
   $("#reset").on("click", function() {
     reset();
   });
-  $('#board li').on('click', function() {
+  $('#board li').on('click', function() { // function that decides what happens when you click on a square in the board
 
-     if(turns == 8) {
-      $(gameInfo).text('Tie - Press the reset button to move to the next game'); // if turns = 8, and no one has won, its a tie
+    let id;
+
+
+     if(turns == 9) {
+      $(gameInfo).text('Tie - Press New Game to move to the next game'); // if turns = 8, and no one has won, its a tie
       $( "li" ).not( "x", "o" ).addClass('disabled');
       turns = 0; //turns reset to 0
     } else if ($(this).hasClass('disabled')){ // if you click on a disabled square, it says the spot has been filled
-      alert("This spot has been filled");
+      alert("You can't play in this spot");
     } else if(turns%2 == 0) { //if turns are an even number e.g. 2, 4, 6, 8, whichever one is clicked on will be filled with o
-      turns++;
+      turns++; //goes to next turn
       $(gameInfo).text('X Turn');
+      id = $(this).attr('id');
+      gameBoard[id] = 'O';
       $(this).text(o); // changes the text to "o"
-      $(this).addClass('disabled o'); // adds class "disabled" to the square which means it is taken
-      if(spot1.hasClass('o')&&spot2.hasClass('o')&&spot3.hasClass('o') ||
-        spot4.hasClass('o')&&spot5.hasClass('o')&&spot6.hasClass('o') ||
-        spot7.hasClass('o')&&spot8.hasClass('o')&&spot9.hasClass('o') ||
-        spot1.hasClass('o')&&spot4.hasClass('o')&&spot7.hasClass('o') ||
-        spot2.hasClass('o')&&spot5.hasClass('o')&&spot8.hasClass('o') ||
-        spot3.hasClass('o')&&spot6.hasClass('o')&&spot9.hasClass('o') ||
-        spot1.hasClass('o')&&spot5.hasClass('o')&&spot9.hasClass('o') ||
-        spot3.hasClass('o')&&spot5.hasClass('o')&&spot7.hasClass('o')) {
+      $(this).addClass('disabled o');
+ // adds class "disabled" to the square which means it is taken
+      // if statements that check winning combinations
+
+      // gameBoard[0] === 'X'
+
+      if(gameBoard[0] === 'O'&&gameBoard[1] === 'O'&&gameBoard[2] === 'O' || // checks all possible combinations for winning from gameBoard array
+        gameBoard[3] === 'O'&&gameBoard[4] === 'O'&&gameBoard[5] === 'O' ||
+        gameBoard[6] === 'O'&&gameBoard[7] === 'O'&&gameBoard[8] === 'O' ||
+        gameBoard[0] === 'O'&&gameBoard[3] === 'O'&&gameBoard[6] === 'O' ||
+        gameBoard[1] === 'O'&&gameBoard[4] === 'O'&&gameBoard[7] === 'O' ||
+        gameBoard[2] === 'O'&&gameBoard[5] === 'O'&&gameBoard[0] === 'O' ||
+        gameBoard[0] === 'O'&&gameBoard[4] === 'O'&&gameBoard[8] === 'O' ||
+        gameBoard[2] === 'O'&&gameBoard[4] === 'O'&&gameBoard[6] === 'O') {
           // checkForWinner();
-          $(gameInfo).text('Winner: O - Press the reset button to move to the next game');
-          $( "li" ).not( "x", "o" ).addClass('disabled');
-          oScore ++;
-          $(scoreOfX).text(`X - ${xScore}`);
+          $(gameInfo).text('Winner: O - Press New Game to move to the next game'); //displays that O has won
+          $( "li" ).not( "x", "o" ).addClass('disabled'); //adds class disabled to any spaces not taken to stop you from continuing to play after a games over
+          oScore ++; // adds 1 to o score
+          $(scoreOfX).text(`X - ${xScore}`); // displays scores
           $(scoreOfO).text(`O - ${oScore}`);
-          game ++;
-          turns = 0;
+          game ++; // goes to next game
+          turns = 0; // resets turns
       }
 
-    } else { // if turns are not even, it will be filled with x
+    } else { // if turns are not even, click will create an x
       turns++;
       $(gameInfo).text('O Turn');
+      id = $(this).attr('id');
+      gameBoard[id] = 'X';
       $(this).text(x); // changes the text to "x"
-      $(this).addClass('disabled x'); // adds class "disabled" to the square which means it is taken
-      if(spot1.hasClass('x')&&spot2.hasClass('x')&&spot3.hasClass('x') ||
-        spot4.hasClass('x')&&spot5.hasClass('x')&&spot6.hasClass('x') ||
-        spot7.hasClass('x')&&spot8.hasClass('x')&&spot9.hasClass('x') ||
-        spot1.hasClass('x')&&spot4.hasClass('x')&&spot7.hasClass('x') ||
-        spot2.hasClass('x')&&spot5.hasClass('x')&&spot8.hasClass('x') ||
-        spot3.hasClass('x')&&spot6.hasClass('x')&&spot9.hasClass('x') ||
-        spot1.hasClass('x')&&spot5.hasClass('x')&&spot9.hasClass('x') ||
-        spot3.hasClass('x')&&spot5.hasClass('x')&&spot7.hasClass('x'))
+      $(this).addClass('disabled x');
+ // adds class "disabled" to the square which means it is taken
+      if(gameBoard[0] === 'X'&&gameBoard[1] === 'X'&&gameBoard[2] === 'X' || // checks all possible combinations for winning from gameBoard array
+        gameBoard[3] === 'X'&&gameBoard[4] === 'X'&&gameBoard[5] === 'X' ||
+        gameBoard[6] === 'X'&&gameBoard[7] === 'X'&&gameBoard[8] === 'X' ||
+        gameBoard[0] === 'X'&&gameBoard[3] === 'X'&&gameBoard[6] === 'X' ||
+        gameBoard[1] === 'X'&&gameBoard[4] === 'X'&&gameBoard[7] === 'X' ||
+        gameBoard[2] === 'X'&&gameBoard[5] === 'X'&&gameBoard[0] === 'X' ||
+        gameBoard[0] === 'X'&&gameBoard[4] === 'X'&&gameBoard[8] === 'X' ||
+        gameBoard[2] === 'X'&&gameBoard[4] === 'X'&&gameBoard[6] === 'X')
       {
         // checkForWinner();
-        $(gameInfo).text('Winner: X - Press the reset button to move to the next game');
-        $( "li" ).not( "x", "o" ).addClass('disabled');
-        xScore ++;
-        $(scoreOfX).text(`X - ${xScore}`);
+        $(gameInfo).text('Winner: X - Press New Game to move to the next game');
+        $( "li" ).not( "x", "o" ).addClass('disabled'); //when the game ends, if any squares are NOT x, or o, it adds the class disabled, which stops you from continuing to play after a game is over
+        xScore ++; // adds 1 to score of x
+        $(scoreOfX).text(`X - ${xScore}`); // displays scores
         $(scoreOfO).text(`O - ${oScore}`);
-        game ++;
-        turns = 0;
+        game ++; // goes to next game
+        turns = 0; // resets turns to 0
     }
   };
 });
